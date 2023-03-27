728440adca31b1c7d94e061a124d85b048379a15
Leonardo Florêncio <leonardo_florenciop@outlook.com>
2023-03-27 06:40:29 -0300

    Projeto já iniciado

---
+++ src/index.js
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import chalk from 'chalk';
+
+function extraiLinks(texto) {
+    const regex = /\[([^[\]]*?)\]\((https?:\/\/[^\s?#.].[^\s]*)\)/gm;
+    const capturas = [...texto.matchAll(regex)];
+    const resultados = capturas.map(captura => ({[captura[1]]: captura[2]}))
+    return resultados
+}
+
+function trataErro(erro) {
+    console.log(erro)
+    throw new Error(chalk.red(erro.code, 'Não há arquivo no diretório'));
+}
+
+//async/await
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const enconding = 'utf-8'
+        const texto = await fs.promises.readFile(caminhoDoArquivo, enconding)
+        console.log(extraiLinks(texto))
+    } catch (erro) {
+        trataErro(erro);
+    } finally {
+        console.log(chalk.yellow('operação concluída'));
+      }
+}
+
+pegaArquivo('./arquivos/texto.md');
